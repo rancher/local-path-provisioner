3d2c168d7f6bd9c0093cd43abc9d53767653e689
Andrei Kvapil <kvapss@gmail.com>
2020-09-21 17:55:01 +0200

    Do not create directory if not found

--- provisioner.go
+++ provisioner.go
@@ -202,7 +202,7 @@ func (p *LocalPathProvisioner) Provision(opts pvController.ProvisionOptions) (*v
 	}
 
 	fs := v1.PersistentVolumeFilesystem
-	hostPathType := v1.HostPathDirectoryOrCreate
+	hostPathType := v1.HostPathDirectory
 	return &v1.PersistentVolume{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: name,
